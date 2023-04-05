a1accaf398c730719de3c44d25d50157ccbc52ab
marrcelosantana <marcelosantana@alu.ufc.br>
2023-04-05 17:42:59 -0300

    [Rick-and-Morty-Web] First Commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,12 @@
+export const defaultTheme = {
+  "white-100": "#fff",
+
+  "body-background": "#202328",
+  "screen-background": "#272b33",
+  "card-background": "#3b3e43",
+  "text-white": "#f5f5f5",
+  "text-gray": "#9e9e9e",
+
+  "green-300": "#55cb44",
+  "red-300": "#d53c2e",
+} as const;
